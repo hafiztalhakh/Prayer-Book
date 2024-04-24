570dbab940153ee0750e0e3629f7ba3c4f18108a
Talha Khalid <hafiz.talhakh@gmail.com>
2024-04-24 12:28:30 +0500

    coming soon

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className='App'>
+      <header className='App-header'>
+        <h1>Prayer Book</h1>
+        <p>Coming Soon</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
